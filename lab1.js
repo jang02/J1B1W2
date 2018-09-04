ac955f8482e9ca014146589679208569521ff1bf
jang02 <jangarretsen01@gmail.com>
2018-09-04 13:05:21 +0200

    Lab 1 en 2

---
+++ lab1.js
@@ -0,0 +1,11 @@
+// Jan Garretsen
+// Applicatie en media ontwikkelaar
+
+
+alert('U moet 3 vragen beantwoorden als u verder wilt op de website'); 
+	document.writeln('Voornaam: ' + prompt('Voer je voornaam in')); // dit zet je voornaam op de pagina
+
+	document.writeln('Achternaam: ' + prompt('Voer je achternaam in')); // dit zet je achternaam op de pagina
+
+	document.writeln('Leeftijd: ' + prompt('Voer je leeftijd in')); // dit zet je leeftijd op de pagina
+
---
+++ lab2.js
@@ -0,0 +1,16 @@
+// Jan Garretsen
+// Applicatie en media ontwikkelaar
+
+
+const popup = "U moet 3 vragen beantwoorden"		//slaat de vraag op
+	alert(popup);									//zorgt ervoor dat de vraag als popup word getoond
+
+var voornaam = prompt('Wat is uw voornaam?');		//slaat de voornaam op
+	document.writeln('Voornaam: ' + voornaam);		//laat de voornaam op de pagina zien
+
+var achternaam = prompt('Wat is uw achternaam?');	//slaat de achternaam op
+	document.writeln('Achternaam: ' + achternaam);	//laat de achternaam zien op de pagina
+
+var leeftijd = prompt('Wat is uw leeftijd?');		//slaat de leeftijd op
+	document.writeln('Leeftijd: ' + leeftijd);		//laat de leeftijd zien op de pagina
+
