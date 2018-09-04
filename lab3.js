46bfa378b0e212d451e8ccb2bc3a89fbabc47cec
jang02 <jangarretsen01@gmail.com>
2018-09-04 14:42:38 +0200

    lab 3

---
+++ lab3.js
@@ -0,0 +1,15 @@
+// Jan Garretsen
+// Applicatie en media ontwikkelaar
+
+var x = 3
+
+document.writeln(1 * x)
+document.writeln(2 * x)
+document.writeln(3 * x)
+document.writeln(4 * x)
+document.writeln(5 * x)
+document.writeln(6 * x)
+document.writeln(7 * x)
+document.writeln(8 * x)
+document.writeln(9 * x)
+document.writeln(10 * x)
