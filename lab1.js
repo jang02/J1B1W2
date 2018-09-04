07be22963fee7f95264b0f950fe695c4b85c8d21
jang02 <jangarretsen01@gmail.com>
2018-09-04 13:13:56 +0200

    testen

--- lab1.js
+++ lab1.js
@@ -8,4 +8,3 @@ alert('U moet 3 vragen beantwoorden als u verder wilt op de website');
 	document.writeln('Achternaam: ' + prompt('Voer je achternaam in')); // dit zet je achternaam op de pagina
 
 	document.writeln('Leeftijd: ' + prompt('Voer je leeftijd in')); // dit zet je leeftijd op de pagina
-
