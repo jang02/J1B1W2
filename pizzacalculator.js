99f5ef49537f608db9f5b030bc957b7509f8674f
jang02 <jangarretsen01@gmail.com>
2018-09-05 11:50:26 +0200

    update
    paar dingen toegevoegd

--- pizzacalculator.js
+++ pizzacalculator.js
@@ -3,17 +3,20 @@
 	Davinci Applicatie Ontwikkelaar
 	Pizza calculator
 */
-var smallamount = parseInt(prompt('Hoeveel kleine pizzas wilt u?'));
+//vraagt de gebruiker hoeveel pizzas ze willen
+var smallamount = parseInt(prompt('Hoeveel kleine pizzas wilt u?'));				
 var mediumamount = parseInt(prompt('Hoeveel medium pizzas wilt u?'));
 var largeamount = parseInt(prompt('Hoeveel grote pizzas wilt u?'));
+//de prijzen van de pizzas
 const small = 2.35;
 const medium = 3.99;
 const large = 5.99;
+//rekent alles uit voor de gebruiker
 var totalsmall = small * smallamount;
 var totalmedium = medium * mediumamount;
 var totallarge = large * largeamount;
 var total = totalsmall + totalmedium + totallarge;
-
+//laat alle informatie op het scherm op een nette manier zien
 document.writeln('Kleine pizzas: ' + smallamount);
 document.writeln('Prijs per kleine pizza: €' + small);
 document.writeln('Totaalprijs kleine pizzas: €' + totalsmall.toFixed(2));
